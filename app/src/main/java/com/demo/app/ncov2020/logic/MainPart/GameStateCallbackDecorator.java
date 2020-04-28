8dd0000355252ddebeed138d7b6f1d154362358a
Mykola Lehkyi <lintey8@gmail.com>
2020-04-28 22:38:17 +0300

    Fix after merge

--- app/src/main/java/com/demo/app/ncov2020/logic/MainPart/GameStateCallbackDecorator.java
+++ app/src/main/java/com/demo/app/ncov2020/logic/MainPart/GameStateCallbackDecorator.java
@@ -131,7 +131,7 @@ public class GameStateCallbackDecorator extends BaseDecorator {
         countryComposite.addComponent(italy);
         countryComposite.addComponent(china);
         Disease disease = new Disease("nCov2019");
-        BaseDecorator baseDecorator = new GameStateLogDecorator(GameStateCallbackDecorator.init(GameStateReali.init(1,"1",countryComposite,disease,new GlobalCure(1000000),new GregorianCalendar(2019,12,31)),new ConcreateCallback()));
+        BaseDecorator baseDecorator = new GameStateLogDecorator(GameStateCallbackDecorator.init(GameStateReali.init(1,"1",countryComposite,disease,new GlobalCure(1000000),new GregorianCalendar(2019,12,31),0),new ConcreateCallback()));
         baseDecorator.addSymptom(new Symptom("Pnevmonia","Hard to breathe",2,4,0));
         baseDecorator.addSymptom(new Symptom("Cough","A-a-a-pchi",2,4,0));
         baseDecorator.addAbility(new Ability("Antibiotics1","Can survive Level1 antibiotics", TypeAbility.ANTIBIOTICS1, new HandlerAntibiotics1()));
